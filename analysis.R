6ddd894d8be3c67705b86d97168c0c672711e920
Ziyi An <ziyia2@uw.edu>
2020-03-01 17:41:39 -0800

    upadted question 2

--- analysis.R
+++ analysis.R
@@ -144,7 +144,7 @@ virus_df_new <- virus_df_new[-c(4, 5, 6, 11, 12, 18, 19, 22, 23), ]
 
 new_date_frame <- data.frame(stock_df, virus_df_new)
 
-reg <- ggplot(data = new_date_frame, mapping = aes(x = as.numeric(reorder(volume, virus_df_new)), y = volume))+
+reg_volume <- ggplot(data = new_date_frame, mapping = aes(x = as.numeric(reorder(volume, virus_df_new)), y = volume))+
   geom_point(color = "red") +
   labs(title = "Regression of volume amount and confirmed cases", 
        x = "Number of confirmed cases", 
